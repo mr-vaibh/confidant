4e8bdab0d64e2e5ae773fd3158c3afdb107a5f1e
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-09-06 20:08:10 +0530

    JWT authentication in rest API

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,6 +5,7 @@ from .views import UserViewSet, ProfileViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 router = DefaultRouter()
@@ -15,4 +16,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,16 +2,17 @@ from rest_framework import viewsets
 from .models import User, Profile
 from .serializers import UserSerializer, ProfileSerializer
 from rest_framework.authentication import BasicAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [BasicAuthentication, JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [BasicAuthentication, JWTAuthentication]
     permission_classes = [IsAuthenticated]
