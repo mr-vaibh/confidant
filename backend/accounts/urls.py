dd72499cc037341b3d4bc7807bfbdc98539cde86
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-10-26 16:08:10 +0530

    account activation

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProfileViewSet
+from .views import UserViewSet, ProfileViewSet, ActivationView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'profiles', ProfileViewSet)
 
 urlpatterns = [
-    path('account/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('account/', include(router.urls)),
+    path('account/activate/<uidb64>/<token>/', ActivationView.as_view(), name='account-activate'),
 ]
