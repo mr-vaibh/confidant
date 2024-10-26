2bd8052b81fbe542408ca4c38b562be12a8dd2ba
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-10-26 14:38:38 +0530

    updated urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,21 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, ProfileViewSet, api_root
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+from .views import ProfileViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'profiles', ProfileViewSet)
 
 urlpatterns = [
-    path('api/', api_root, name='api_root'),
-    path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('account/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
