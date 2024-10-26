1736b8db393421c2b87921793a2ad54b5de927db
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-10-26 16:16:13 +0530

    removed unwanted code

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -14,12 +14,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-class UserDetailView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response({'username': request.user.username})
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
