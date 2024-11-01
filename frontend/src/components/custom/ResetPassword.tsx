d1899e7cf4a5ef840281424f7d1ecba1f1a51c1b
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-11-02 04:24:57 +0530

    removed unused module

--- frontend/src/components/custom/ResetPassword.tsx
+++ frontend/src/components/custom/ResetPassword.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { AuthActions } from "@/app/auth/utils";
 
-import SpinningLoader from "./SpinningLoader";
-
 type FormData = {
     email: string;
 };
@@ -37,7 +35,7 @@ const ResetPassword = () => {
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg w-1/3">
+            <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
                 <h3 className="text-2xl font-semibold">Reset Password</h3>
                 <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
                     <label className="block" htmlFor="email">
