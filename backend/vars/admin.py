1625cc0773bc416b5e3b200a7769c8f706f05ee7
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2025-03-29 15:19:38 +0530

    default created_by value in VariableVersionInline

--- backend/vars/admin.py
+++ backend/vars/admin.py
@@ -1,6 +1,31 @@
-# vars/admin.py
 from django.contrib import admin
+from django import forms
 from .models import EnvironmentVariable, VariableVersion
+
+# Custom formset for VariableVersion
+class VariableVersionInlineFormSet(forms.models.BaseInlineFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Access the request object and set created_by to the user of the related EnvironmentVariable
+        for form in self.forms:
+            if form.instance.pk is not None:  # Check if it's a new instance (not yet saved)
+                environment_variable = form.instance.variable
+                if environment_variable:  # Ensure there's a valid related EnvironmentVariable
+                    form.initial['created_by'] = environment_variable.created_by
+
+# Inline for VariableVersion
+class VariableVersionInline(admin.TabularInline):
+    model = VariableVersion
+    extra = 0  # No extra empty rows
+    fields = ('version', 'value', 'created_at', 'created_by')
+    readonly_fields = ('created_at', 'created_by')
+    formset = VariableVersionInlineFormSet  # Use the custom formset
+
+    def get_formset_kwargs(self, request, obj=None, **kwargs):
+        # Pass the request object to the formset so we can use it in the form initialization
+        kwargs = super().get_formset_kwargs(request, obj, **kwargs)
+        kwargs['request'] = request
+        return kwargs
 
 @admin.register(EnvironmentVariable)
 class EnvironmentVariableAdmin(admin.ModelAdmin):
@@ -20,19 +45,12 @@ class EnvironmentVariableAdmin(admin.ModelAdmin):
         }),
     )
 
-    # Display related VariableVersions inline
-    class VariableVersionInline(admin.TabularInline):
-        model = VariableVersion
-        extra = 0  # No extra empty rows
-        fields = ('version', 'value', 'created_at', 'created_by')
-        readonly_fields = ('created_at', 'created_by')
-
     inlines = [VariableVersionInline]
 
-    # Save the created_by as the current user
+    # Save the created_by as the current user when creating the EnvironmentVariable
     def save_model(self, request, obj, form, change):
         if not change:
-            obj.created_by = request.user
+            obj.created_by = request.user  # Set created_by to the current user when creating EnvironmentVariable
         obj.save()
 
 @admin.register(VariableVersion)
@@ -40,8 +58,8 @@ class VariableVersionAdmin(admin.ModelAdmin):
     list_display = ('id', 'variable', 'version', 'value', 'created_at', 'created_by')
     list_filter = ('created_by', 'created_at')
     search_fields = ('variable__name', 'version', 'value')
-    readonly_fields = ('variable', 'version', 'created_by', 'created_at')
-    
+    readonly_fields = ('variable', 'version', 'created_at', 'created_by')
+
     # Organize fields in sections
     fieldsets = (
         ("Version Information", {
@@ -51,9 +69,9 @@ class VariableVersionAdmin(admin.ModelAdmin):
             'fields': ('created_by', 'created_at')
         }),
     )
-    
-    # Set created_by automatically when saving a new version
+
+    # Save the created_by as the current user when creating a new version
     def save_model(self, request, obj, form, change):
         if not change:
-            obj.created_by = request.user
+            obj.created_by = obj.variable.created_by  # Set created_by to the created_by of the related EnvironmentVariable
         obj.save()
